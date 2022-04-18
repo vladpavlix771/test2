c2133bd5bffadfbaec16613ab177798e603775e5
vladpavlix771 <56918679+vladpavlix771@users.noreply.github.com>
2022-04-18 10:32:43 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-bot_token = "1284008799:AAHkBJn1Rjlt6aI64HOxnAV15Sbk607mLNs"
+bot_token = "5290095542:AAFSl2hx4daq1DpMzvlhTqwh697QKWlv5DA"
 qiwi_number = "+79967063255"
 bitcoin_adress = "1324930511:AAH4E9IrvLWtsEPTHiM8BAbpS7r0998BFds"
